5fd68e92f1bb2711cd99af55ade9e11c9165cecb
Atish Chandra <48754885+atishchandra@users.noreply.github.com>
2020-07-29 12:40:30 +0530

    Create Swap Two Variables.py

---
+++ Swap Two Variables.py
@@ -0,0 +1,10 @@
+#Swap Two Variables
+var1=int(input("Enter the first value: "))
+var2=int(input("Enter the second value: "))
+print("Before Swapping: ")
+print("Var1= ",var1)
+print("Var2= ",var2)
+var1,var2=var2,var1
+print("After Swapping: ")
+print("Var1= ",var1)
+print("Var2= ",var2)
